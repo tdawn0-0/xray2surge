5910ab1fcb1bbcf81f355f9a886b3b84b5785adb
Jyeu <chen@jyeu.xyz>
2026-01-19 21:08:32 +0800

    Remove watch, ignore_watch, and environment configurations from PM2 ecosystem file.

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,13 +3,5 @@ module.exports = {
     name: "xray2surge",
     script: "index.ts",
     interpreter: "bun",
-    watch: true,
-    ignore_watch: ["node_modules", "xray.json", ".git"],
-    env: {
-      NODE_ENV: "development",
-    },
-    env_production: {
-      NODE_ENV: "production",
-    }
   }]
 };
